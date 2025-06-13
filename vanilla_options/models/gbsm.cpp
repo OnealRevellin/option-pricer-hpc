784bab9b2ce124a63b180eb45a861bc4ee9530ac
OnealRevellin <oneal.revellin-falcoz@proton.me>
2025-06-13 22:17:54 +0200

    gbsm cpp -> pushed init function

--- vanilla_options/models/gbsm.cpp
+++ vanilla_options/models/gbsm.cpp
@@ -1,4 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+vector<double> gbsm_value(
+    vector<bool> &is_call,
+    vector<double> &S,
+    vector<double> &K,
+    vector<double> &T,
+    vector<double> &r,
+    vector<double> &sigma,
+    vector<double> &b
+)
+{
+    vector<double> values(S.size());
+    for (size_t i = 0; i < S.size(); ++i)
+    {
+        if (T[i] <= 0.0)
+        {
+            values[i] = is_call[i] ? max(S[i] - K[i], 0.0) : max(K[i] - S[i], 0.0);
+        }
+    }
+
+    return values;
+}
 
 int main()
 {
