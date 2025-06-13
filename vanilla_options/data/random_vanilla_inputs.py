c325c3b65f10550075ec85c87b93ca865573bef4
OnealRevellin <oneal.revellin-falcoz@proton.me>
2025-06-13 20:14:32 +0200

    1M rows CSV with random vanilla options inputs

---
+++ vanilla_options/data/random_vanilla_inputs.py
@@ -0,0 +1,25 @@
+"""
+Generate a csv containing random inputs for vanilla options pricing.
+"""
+
+import numpy as np
+import pandas as pd
+
+nb_sims = 1_000_000
+S = np.random.normal(loc=100.0, scale=0.17, size=nb_sims)
+K = np.random.normal(loc=100.0, scale=0.17, size=nb_sims)
+T = np.random.uniform(0.0, 5.0, nb_sims)
+r = np.full(nb_sims, 0.04)
+sigma = np.random.normal(loc=0.20, scale=0.04, size=nb_sims)
+q = np.zeros(nb_sims)
+
+df = pd.DataFrame({
+    'S': S,
+    'K': K,
+    'T': T,
+    'r': r,
+    "sigma": sigma,
+    'q': q
+})
+
+df.to_csv("data/vanilla_inputs.csv", encoding="utf-8", index=False)
