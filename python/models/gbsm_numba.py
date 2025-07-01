3477785e250f8e7dd9d693c84c8c301d063c15be
OnealRevellin <oneal.revellin-falcoz@proton.me>
2025-07-01 16:19:59 +0200

    style: added docstrings and __main__ func

--- python/models/gbsm_numba.py
+++ python/models/gbsm_numba.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numba import njit, prange
 import math
+import time
 
 @njit
 def norm_cdf(x):
@@ -22,6 +23,44 @@ def gbsm_numba_value(
     sigma: float, 
     b: float
 ) -> float:
+    """
+    Generalized Black Scholes Merton pricing model using Numba for acceleration.
+
+    This function is decorated with @njit(parallel=True, fastmath=True) to leverage Numba's
+    Just-In-Time (JIT) compilation capabilities, which significantly speeds up execution by:
+
+    - Compiling Python code to optimized machine code, eliminating interpreter overhead.
+    - Enabling automatic parallelization of loops across multiple CPU cores (parallel=True),
+    allowing concurrent computation for large input arrays.
+    - Activating fast math optimizations (fastmath=True) that allow more aggressive
+    floating-point arithmetic transformations for improved performance with minimal
+    precision loss.
+
+    Parameters
+    --------------------
+    flavor : string
+        Option type ("Call" or "Put").
+    S : float or np.ndarray
+        Underlying price.
+    K : float or np.ndarray
+        Strike price.
+    T : float or np.ndarray
+        Time to maturity (in years).
+    r : float or np.ndarray
+        Risk-free interest rate (annualized).
+    sigma : float or np.ndarray
+        Volatility (annualized in %).
+    b : float or np.ndarray
+        Cost-of-carry.
+    --------------------
+
+    Using this model, b can be adapted so the model can match the underlying type.
+    For example :
+    b = r   ==> Black Scholes model for stock options.
+    b = 0.0 ==> Black76 model for options on futures.
+    b = r - q ==> Black-Scholes-Merton model for stock options with continuous
+        dividend yield.
+    """
     N = len(S)
     output = np.empty(N, dtype=np.float64)
     for i in prange(N):
@@ -47,3 +86,24 @@ def gbsm_numba_value(
         output[i] = (is_call * call_value + is_put * put_value)
     
     return output
+
+
+if __name__ == '__main__':
+    N = 50_000_000
+
+    flavor = np.random.choice(["Call", "Put"], size=N)
+    S = np.random.uniform(50, 150, N).astype(np.float64)
+    K = np.random.uniform(50, 150, N).astype(np.float64)
+    T = np.random.uniform(0.01, 2.0, N).astype(np.float64)
+    r = np.random.uniform(0.0, 0.1, N).astype(np.float64)
+    sigma = np.random.uniform(0.1, 0.5, N).astype(np.float64)
+    b = np.zeros(N).astype(np.float64)
+
+    start_ns = time.perf_counter_ns()
+    prices = gbsm_numba_value(flavor, S, K, T, r, sigma, b)
+    end_ns = time.perf_counter_ns()
+    exec_time_ns = end_ns - start_ns
+    print(f"Execution time: {exec_time_ns} ns")
+    print(f"Execution time per option: {exec_time_ns / N} ns")
+
+    print(prices[:10])
