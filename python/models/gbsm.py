9cd1f4b792fd3b0d7a05a4d7ce4de2f4651bb556
OnealRevellin <oneal.revellin-falcoz@proton.me>
2025-07-01 15:50:29 +0200

    chore: added pricing run and exec timing

--- python/models/gbsm.py
+++ python/models/gbsm.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from scipy.stats import norm
-
+import time
 
 def gbsm_value(
     flavor: str, 
@@ -44,17 +44,35 @@ def gbsm_value(
     d1: float = (np.log(S / K) + (b + sigma**2 / 2) * T) / (sigma * sqrtT)
     d2: float = d1 - sigma * sqrtT
 
-    if flavor == "Call":
-        return (
+    call = (
             S * np.exp((b - r) * T) * norm.cdf(d1)
             - K * np.exp(-r * T) * norm.cdf(d2)
         )
-    elif flavor == 'Put':
-        return (
+    
+    put = (
             K * np.exp(-r * T) * norm.cdf(-d2)
             - S * np.exp((b - r) * T) * norm.cdf(-d1)
         )
-    else:
-        raise ValueError("Flavor is not recognized. Please input 'Call' or 'Put'.")
     
+    price = np.where(flavor == 'Call', call, put)
+    return price
     
+if __name__ == '__main__':
+    N = 50_000_000
+
+    flavor = np.random.choice(["Call", "Put"], size=N)
+    S = np.random.uniform(50, 150, N).astype(np.float64)
+    K = np.random.uniform(50, 150, N).astype(np.float64)
+    T = np.random.uniform(0.01, 2.0, N).astype(np.float64)
+    r = np.random.uniform(0.0, 0.1, N).astype(np.float64)
+    sigma = np.random.uniform(0.1, 0.5, N).astype(np.float64)
+    b = np.zeros(N).astype(np.float64)
+
+    start_ns = time.perf_counter_ns()
+    prices = gbsm_value(flavor, S, K, T, r, sigma, b)
+    end_ns = time.perf_counter_ns()
+    exec_time_ns = end_ns - start_ns
+    print(f"Execution time: {exec_time_ns} ns")
+    print(f"Execution time per option: {exec_time_ns / N} ns")
+
+    print(prices[:10])
